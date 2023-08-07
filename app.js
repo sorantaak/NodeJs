7dc54cd2a91527a3a5ba44810bf8b5b9eb0389d0
soran <soran.taak@gmail.com>
2023-08-07 10:43:22 +0330

    OS Module

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
-const path = require("path");
-const pathObject = path.parse(__filename);
-console.log([pathObject]);
+const os = require("node:os");
+var totalMemory = os.totalmem;
+var freeMemory = os.freemem;
+console.log(`Total Memory: ${totalMemory} - Free Memory: ${freeMemory}`);
+// console.log("Free Memory: " + os.freemem);
