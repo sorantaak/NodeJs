04b26b1e93978d566e4c6df015ce91be9f04a089
soran <soran.taak@gmail.com>
2023-08-07 09:06:16 +0330

    Module

--- app.js
+++ app.js
@@ -1,12 +1,1 @@
-// console.log(); // global object
-
-// setTimeout();
-// clearTimeout();
-
-// setInterval();
-// clearInterval();
-
-var message ='';
-// window.message
-global.console.log('test')
-global.console.log(global.message)
+console.log(module);
