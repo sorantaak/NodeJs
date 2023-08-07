e88351f8726849011fb3b93c93824b84d282fd2b
soran <soran.taak@gmail.com>
2023-08-07 09:56:36 +0330

    Module Wrapper Function

--- logger.js
+++ logger.js
@@ -1,7 +1,11 @@
-var url = "http://google.com";
-function log(message) {
-	console.log(message);
-}
-// module.exports.log = log;
-module.exports = log; // in this line export directly log function
-// module.exports.endPoinUrl = url;
+// (function (exports, require, module, __filename, __dirname) {
+    console.log(__filename);
+    console.log(__dirname);
+	var url = "http://google.com";
+	function log(message) {
+		console.log(message);
+	}
+	// module.exports.log = log;
+	module.exports = log; // in this line export directly log function
+	// module.exports.endPoinUrl = url;
+// });
