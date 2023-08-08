c5c31bbf7ecf70c8a6cf967732f1d27800e9ede2
soran <soran.taak@gmail.com>
2023-08-08 07:43:35 +0330

    Event Arguments

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const EventEmitter = require("node:events");
 const emitter = new EventEmitter();
 
 // Register a Listener
-emitter.on("messageLogged", () => {
-	console.log("Listener Called");
+emitter.on("messageLogged", (args) => {
+	console.log("Listener Called", args);
 });
 
 // Raise an Event
-emitter.emit("messageLogged");
+emitter.emit("messageLogged", { id: 1, url: "http://" });
+//Raise : Logging (data:message)
